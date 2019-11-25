e463bdd6e803521f2ccb626f660f95ec25611e01
sithelo ngwenya <sithelo.ngwenya@rmb.co.za>
2019-11-25 18:18:32 +0200

    cleanup mentee model added

--- src/app/core/model/mentee.ts
+++ src/app/core/model/mentee.ts
@@ -1,6 +1,52 @@
+export class MenteeDomianArea {
+  MenteeDomainId: number;
+  MenteeId: number;
+  DomainId: number;
+  CreatedDatetime: Date;
+  TrackDatetime: Date;
+}
+
+export class MenteeExperience {
+  MenteeExperienceId: number;
+  MenteeId: number;
+  ExperienceId: number;
+  CreatedDateTime: Date;
+  TrackDateTime: Date;
+}
+
 export class Mentee {
-  id: number;
-  name: string;
-  city: string;
-  orderTotal: number;
+  MenteeId: number;
+  EmployeeId: string;
+  InDivision: boolean;
+  Division: string;
+  TenantId?: any;
+  Interest: string;
+  ServicePeriod: number;
+  Duration: number;
+  UnitOfTimeId: number;
+  YearsOfExperience: number;
+  PreferredMentorId: number;
+  PreferredMentorEmpId?: any;
+  PreferredMentorGenderId: number;
+  PreferredMentorAgeId: number;
+  ShareProfile: boolean;
+  ReadTerms: boolean;
+  Comment: string;
+  CreatedDate: Date;
+  MenteeDomianArea: MenteeDomianArea[];
+  MenteeExperience: MenteeExperience[];
+  UnitOfTimes?: any;
+  Experiences?: any;
+  DomainAreas?: any;
+  Gender?: any;
+  AgePreferences?: any;
+}
+
+export class MenteeList {
+  results: Mentee[];
+  errors: any[];
+  TotalItems: number;
+  PageNumber: number;
+  PageSize: number;
+  PageCount: number;
 }
